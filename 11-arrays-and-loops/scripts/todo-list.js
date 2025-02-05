c7ace4e8f0382fe18a651a1b135c595925b2cfbb
MaratFa <evolution99@rambler.ru>
2025-02-05 07:10:12 +0300

    edited

--- 11-arrays-and-loops/scripts/todo-list.js
+++ 11-arrays-and-loops/scripts/todo-list.js
@@ -1,12 +1,26 @@
-const todoList = ["make dinner", "wash dishes"];
+const todoList = [
+  { name: "make dinner", dueDate: "2025-02-02" },
+  { name: "wash dishes", dueDate: "2025-02-02" },
+];
 
 function renderTodoList() {
   let todoListHTML = "";
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const name = todoList[i].name;
+    const dueDate = todoList[i].dueDate;
+    const html = `
+      <p>
+        ${name} ${dueDate}
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        ">Delete</button>
+      </p>
+    `;
     todoListHTML += html;
+    console.log(todoListHTML);
   }
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
