c7d4ffacad36df9e8a151e79e078616edb0f6310
MaratFa <evolution99@rambler.ru>
2025-02-06 07:49:16 +0300

    edited

--- 11-arrays-and-loops/scripts/todo-list.js
+++ 11-arrays-and-loops/scripts/todo-list.js
@@ -12,13 +12,12 @@ function renderTodoList() {
     // const dueDate = todoList[i].dueDate;
     const { name, dueDate } = todoObject;
     const html = `
-      <p>
-        ${name} ${dueDate}
-        <button onclick="
-          todoList.splice(${i}, 1);
-          renderTodoList();
-        ">Delete</button>
-      </p>
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      ">Delete</button>
     `;
     todoListHTML += html;
   }
