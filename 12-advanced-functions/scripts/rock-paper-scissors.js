82a2c8f2a5976b174ed21af7ff6f525476c25c2d
MaratFa <evolution99@rambler.ru>
2025-02-11 06:55:40 +0300

    edited

--- 12-advanced-functions/scripts/rock-paper-scissors.js
+++ 12-advanced-functions/scripts/rock-paper-scissors.js
@@ -24,6 +24,24 @@ function autoPlay() {
   }
 }
 
+document
+  .querySelector(".js-rock-button")
+  .addEventListener("click", () => playGame("rock"));
+
+document
+  .querySelector(".js-paper-button")
+  .addEventListener("click", () => playGame("paper"));
+
+document
+  .querySelector(".js-scissors-button")
+  .addEventListener("click", () => playGame("scissors"));
+
+document.querySelector("body").addEventListener("keydown", (event) => {
+  if (event.key === "r") playGame("rock");
+  if (event.key === "p") playGame("paper");
+  if (event.key === "s") playGame("scissors");
+});
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
