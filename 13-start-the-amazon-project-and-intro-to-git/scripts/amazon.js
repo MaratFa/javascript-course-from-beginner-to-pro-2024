47d5fbcd6962666f11e277203ec1c580dafc09e4
MaratFa <evolution99@rambler.ru>
2025-02-13 08:50:46 +0300

    edited

--- 13-start-the-amazon-project-and-intro-to-git/scripts/amazon.js
+++ 13-start-the-amazon-project-and-intro-to-git/scripts/amazon.js
@@ -50,11 +50,16 @@ products.forEach((product) => {
               Added
             </div>
 
-            <button class="add-to-cart-button button-primary">Add to Cart</button>
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${
+              product.name
+            }">Add to Cart</button>
           </div>
         `;
 });
 
-console.log(productsHTML);
-
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    console.log(button.dataset.productName);
+  });
+});
