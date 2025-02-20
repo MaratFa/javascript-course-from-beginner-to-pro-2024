da3587abc3c0c4d4b2bae78a97b132ede36b01b5
MaratFa <evolution99@rambler.ru>
2025-02-20 07:38:00 +0300

    edited

--- 15-external-libraries/scripts/checkout.js
+++ 15-external-libraries/scripts/checkout.js
@@ -1,11 +1,8 @@
 import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
-import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
-
-hello();
 
 const today = dayjs();
 const deliveryDate = today.add(7, "days");
@@ -62,14 +59,14 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                ${deliveryOptionsHTML(matchingProduct)}
+                ${deliveryOptionsHTML(matchingProduct, cartItem)}
               </div>
             </div>
           </div>
   `;
 });
 
-function deliveryOptionsHTML(matchingProduct) {
+function deliveryOptionsHTML(matchingProduct, cartItem) {
   let html = "";
 
   deliveryOptions.forEach((deliveryOption) => {
@@ -80,23 +77,27 @@ function deliveryOptionsHTML(matchingProduct) {
     const priceString =
       deliveryOption.priceCents === 0
         ? "FREE"
-        : `${formatCurrency(deliveryOption.priceCents)} - `;
+        : `${formatCurrency(deliveryOption.priceCents)} -`;
+
+        console.log(cartItem);
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
     html += `
       <div class="delivery-option">
         <input
           type="radio"
+          ${isChecked ? "checked" : ""}
           class="delivery-option-input"
           name="delivery-option-${matchingProduct.id}"
         />
         <div>
           <div class="delivery-option-date">${dateString}</div>
-          <div class="delivery-option-price">${priceString} - Shipping</div>
+          <div class="delivery-option-price">${priceString} Shipping</div>
         </div>
       </div>
     `;
   });
 
-  console.log(html);
   return html;
 }
 
