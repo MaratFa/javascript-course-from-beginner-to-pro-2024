5adc855933617e6a5816179d3fe59b59033b0d70
MaratFa <evolution99@rambler.ru>
2025-03-06 09:23:46 +0300

    Edited

--- 19-classes/data/products.js
+++ 19-classes/data/products.js
@@ -10,7 +10,7 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
-class Products {
+class Product {
   id;
   image;
   name;
@@ -18,11 +18,23 @@ class Products {
   priceCents;
 
   constructor(productDetails) {
-    this.id = "";
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+
   }
 }
 
-const product1 = new Products({
+const product1 = new Product({
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
   name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -506,4 +518,6 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails) => new Product(productDetails));
+
+console.log(products);
