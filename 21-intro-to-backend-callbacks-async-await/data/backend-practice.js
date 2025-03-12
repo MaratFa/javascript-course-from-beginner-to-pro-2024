69128684998f7802f82174b413ccd6fcb909cfa4
MaratFa <evolution99@rambler.ru>
2025-03-12 07:20:30 +0300

    Edited

--- 21-intro-to-backend-callbacks-async-await/data/backend-practice.js
+++ 21-intro-to-backend-callbacks-async-await/data/backend-practice.js
@@ -1,2 +1,9 @@
 const xhr = new XMLHttpRequest();
-xhr.open('get', );
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.response);
+
+});
+
+xhr.open("get", "https://supersimplebackend.dev/images/apple.jpg");
+xhr.send();
