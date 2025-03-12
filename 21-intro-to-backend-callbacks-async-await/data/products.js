bcf91193278f50e921080b7ca611a4a0ceb3ed33
MaratFa <evolution99@rambler.ru>
2025-03-12 10:45:33 +0300

    Edited

--- 21-intro-to-backend-callbacks-async-await/data/products.js
+++ 21-intro-to-backend-callbacks-async-await/data/products.js
@@ -56,39 +56,26 @@ class Clothing extends Product {
   }
 }
 
-/*
-const date = new Date();
-console.log(date);
-date.toLocaleTimeString();
-console.log(date.toLocaleTimeString());
-*/
+export let products = [];
 
-/*
-console.log(this);
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log("load products");
 
-const object2 = {
-  a: 2,
-  b: this.a,
-};
-console.log(object2);
-*/
-
-/*
-function logThis() {
-  console.log(this);
+    fun();
+  });
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
 }
 
-logThis();
-logThis.call('hello');
-
-const object3 = {
-  method: () => {
-    console.log(this);
-  }
-}
-object3.method();
-*/
-
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -567,3 +554,4 @@ export const products = [
 
   return new Product(productDetails);
 });
+*/
